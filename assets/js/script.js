3533c5dd6e15efe7356689eef4ead1275c427ddc
Michael Bair <mbair87@gmail.com>
2021-12-03 14:06:49 -0700

    added function to save tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@
 var currentDay = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(currentDay);
 
+// add save function
+    $(".saveBtn").on("click", function () {
+        
+        //obtain values from time blocks
+        var task = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // save tasks to local storage
+        localStorage.setItem(time, task);
+    })
+
+//time keeping 
 function clock() {
     //get current hour
     var currentTime = moment().hour();
@@ -12,16 +24,19 @@ function clock() {
 
         // check if hour is past, present or future and adjust classes accordingly
 
+        //past
         if (timeBlock < currentTime) {
             $(this).removeClass("future");
             $(this).removeClass("present");
             $(this).addClass("past");
         }
+        //present
         else if (timeBlock === currentTime) {
             $(this).removeClass("past");
             $(this).removeClass("future");
             $(this).addClass("present");
-        }
+        } 
+        //future
         else {
             $(this).removeClass("present");
             $(this).removeClass("past");
@@ -29,5 +44,7 @@ function clock() {
 
         }
     })
-}
-clock();
+
+
+
+} clock();
